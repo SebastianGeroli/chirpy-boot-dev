fba03dd546eb18ab695fd40533ec8ce432e7300d
SebastianGeroli <sebastiangeroli@gmail.com>
2026-09-14 21:23:15 +0000

    Add index.html

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 
 func main() {
 	serveMux := http.ServeMux{}
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
 		Handler: &serveMux,
 		Addr:    ":8080",
 	}
 	server.ListenAndServe()
-
 }
